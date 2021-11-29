4ce2ac4be1491d1e95cdd173d38b614ae186903f
jihun-yang <jihunyang@daum.net>
2021-11-29 14:52:21 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
